99d5f9c1e70f8d8588bf30ce1e0c16c4c9bb97a3
hjc2000 <hjc.666@foxmail.com>
2024-09-24 14:47:34 +0800

    Ticker.Test()

--- main.lua
+++ main.lua
@@ -1,7 +1,30 @@
 require("Math.InertialElement")
+require("Ticker")
 
-local inertial_element = Math.InertialElement.New(0.05, 0.010, 0.1)
-for i = 0, 100 do
-	Math.InertialElement.Input(inertial_element, 5)
-	print(Math.InertialElement.CurrentOutput(inertial_element))
+
+function Math.InertialElement.Test()
+	local inertial_element = Math.InertialElement.New(0.05, 0.010, 0.1)
+	for i = 0, 100 do
+		Math.InertialElement.Input(inertial_element, 5)
+		print(Math.InertialElement.CurrentOutput(inertial_element))
+	end
 end
+
+-- Math.InertialElement.Test()
+
+
+function Ticker.Test()
+	local ticker = Ticker.New(
+		10,
+		1000,
+		function()
+			print("timeup")
+		end
+	)
+
+	for i = 1, 1000 do
+		Ticker.Tick(ticker)
+	end
+end
+
+Ticker.Test()
