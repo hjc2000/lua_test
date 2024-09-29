dca03a9a3a147ea917fe3f73c21305d13797c385
hjc2000 <hjc.666@foxmail.com>
2024-09-29 09:21:32 +0800

    print_env(_ENV)

--- main.lua
+++ main.lua
@@ -1,50 +1,9 @@
-require("Math.InertialElement")
-require("Ticker")
-
-
-function Math.InertialElement.Test()
-	local inertial_element = Math.InertialElement.New(0.05, 0.010, 0.1)
-	for i = 0, 100 do
-		Math.InertialElement.Input(inertial_element, 5)
-		print(Math.InertialElement.CurrentOutput(inertial_element))
+-- 打印_ENV的内容
+function print_env(env)
+	for key, value in pairs(env) do
+		print(key .. ": " .. tostring(value))
 	end
 end
 
--- Math.InertialElement.Test()
-
-
-function Ticker.Test()
-	local ticker = Ticker.New(
-		10,
-		1000,
-		function()
-			print("timeup")
-		end
-	)
-
-	for i = 1, 1000 do
-		Ticker.Tick(ticker)
-	end
-end
-
--- Ticker.Test()
-
-if (true) then
-	local str = "666"
-
-	function Log1()
-		print(str)
-	end
-end
-
-
-if (true) then
-	local str = "777"
-
-	function Log2()
-		print(str)
-	end
-end
-
-Log1()
-Log2()
+-- 调用函数打印_ENV
+print_env(_ENV)
