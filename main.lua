6122546459ea8b0f675043a70a9df4f86eaf161d
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-10-02 00:14:41 +0800

    辅助JCNET的开发

--- main.lua
+++ main.lua
@@ -1,9 +1,6 @@
--- 打印_ENV的内容
-local function print_env(env)
-	for key, value in pairs(env) do
-		print(key)
-	end
-end
-
--- 调用函数打印_ENV
-print_env(_ENV)
+G = {}
+G.T1 = {}
+G.T2 = {}
+G.T1.name = 6
+G[0] = "hello world"
+_G[0] = "666"
