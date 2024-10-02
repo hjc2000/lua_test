ab3a0d4a3e039056ae8e0e912ec10388f4257e94
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-10-02 23:33:36 +0800

    Person = {}

--- main.lua
+++ main.lua
@@ -1,6 +1,15 @@
-G = {}
-G.T1 = {}
-G.T2 = {}
-G.T1.name = 6
-G[0] = "hello world"
-_G[0] = "666"
+Person = {}
+Person[0] = {}
+Person[0].Name = "张三"
+Person[0].Age = 20
+Person[0].School = "大学"
+
+Person[0].Child = {}
+Person[0].Child.Name = "小明"
+Person[0].Child.Age = 20
+Person[0].Child.School = "小学"
+
+Person[0].Child.Child = {}
+Person[0].Child.Child.Name = "小红"
+Person[0].Child.Child.Age = 20
+Person[0].Child.Child.School = "中学"
