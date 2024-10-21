aadc5f11bcdef60bff9e16772967526efa3ba159
hjc2000 <hjc.666@foxmail.com>
2024-10-21 11:27:20 +0800

    Array

--- Detector/StaticFrictionDetector.lua
+++ Detector/StaticFrictionDetector.lua
@@ -4,10 +4,6 @@ if true then
 		StaticFrictionDetector = {}
 	end
 
-
-	--- 静摩擦检测结果
-	local _static_friction = 0
-
 	--- 在速度模式下以指定的转矩限制值转动
 	--- @param torque integer 转矩限制值
 	local function RunWithTorque(torque)
@@ -16,6 +12,7 @@ if true then
 		Servo.SetSpeedAndRun(100)
 	end
 
+	--- 将转矩限制值放开到 100%，然后将速度设置为 0，让伺服快速停下来，停下来后才能进行下一次的静摩擦检测。
 	local function Stop()
 		Servo.Param.SetBothTorqueLimit(100)
 		Servo.Stop()
@@ -26,33 +23,18 @@ if true then
 
 
 	--- 使用二分法，首先指定转矩限制值的区间左右端点，在区间内寻找静摩擦对应的转矩
-	local _left_torque = 0
-	local _right_torque = 100
 	local _detecte_result = 0
 
-	--- 执行检测
-	function StaticFrictionDetector.Detecte()
-		_left_torque = 0
-		_right_torque = 100
+	--- 检测一次静摩擦转矩
+	--- @return integer
+	local function DetecteOnce()
+		local left_torque = 0
+		local right_torque = 100
 
 		while true do
-			local current_torque = (_left_torque + _right_torque) // 2
-			if (_left_torque >= _right_torque) then
-				_detecte_result = current_torque
-				print("检测结束 current_torque <= _left_torque，静摩擦： ", _detecte_result)
-				return
-			end
-
-			if (current_torque <= _left_torque) then
-				_detecte_result = current_torque
-				print("检测结束 current_torque <= _left_torque，静摩擦： ", _detecte_result)
-				return
-			end
-
-			if (current_torque >= _right_torque) then
-				_detecte_result = current_torque
-				print("检测结束 current_torque <= _left_torque，静摩擦： ", _detecte_result)
-				return
+			local current_torque = (left_torque + right_torque) // 2
+			if (left_torque >= right_torque) then
+				return current_torque
 			end
 
 			RunWithTorque(current_torque)
@@ -60,14 +42,26 @@ if true then
 
 			if (Servo.Feedback.Speed() > 10) then
 				-- _current_torque 已经让伺服转起来了，说明大于静摩擦
-				_right_torque = current_torque
+				right_torque = current_torque - 1
 			else
 				-- 没让伺服转起来，说明 _current_torque 无法克服静摩擦
-				_left_torque = current_torque
+				left_torque = current_torque + 1
 			end
 
 			Stop()
 		end
+	end
+
+	--- 执行检测
+	function StaticFrictionDetector.Detecte()
+		local torque_arr = {}
+		for i = 1, 10, 1 do
+			torque_arr[i] = DetecteOnce()
+		end
+
+		torque_arr = Array.RemoveMinMax(torque_arr)
+		_detecte_result = Array.CalculateAverage(torque_arr)
+		print("检测结束，静摩擦： ", _detecte_result)
 	end
 
 	--- 检测结果
