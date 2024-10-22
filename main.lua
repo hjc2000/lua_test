83907ad1f2541a1ebd2c461626125a18c32d8da3
hjc2000 <hjc.666@foxmail.com>
2024-10-22 10:03:37 +0800

    TestFreeStopping

--- main.lua
+++ main.lua
@@ -10,6 +10,32 @@ local function TestTorqueMode()
 	Servo.SetTorqueAndRun(10)
 end
 
+--- 测试电机自由停车
+local function TestFreeStopping()
+	Servo.ChangeToSpeedMode()
+	Servo.Param.SetBothTorqueLimit(20)
+	Servo.SetSpeedAndRun(300)
+
+	-- 等待直到电机转起来，达到指定速度
+	Detector.AccelerationDetector.Reset()
+
+	--- 计数，至少向滤波器输入 100 个点
+	local count = 0
+	while true do
+		Detector.AccelerationDetector.Detect()
+		if (math.abs(Detector.AccelerationDetector.Acceleration()) == 0 and
+				Servo.Feedback.Speed() >= 100 and
+				count > 100) then
+			break
+		end
+
+		Servo.Timer.Delay(Servo.Timer.Period())
+		count = count + 1
+	end
+
+	Servo.Param.SetBothTorqueLimit(0)
+end
+
 --- 初始化
 if true then
 	Servo.Configure()
@@ -22,6 +48,7 @@ if true then
 
 	Detector.StaticFrictionDetector.Detecte()
 	-- TestTorqueMode()
+	-- TestFreeStopping()
 end
 
 local _max_time = 0
